01af43f90083bdcf3711f74247b36252ced32c48
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2016-04-29 22:03:59 +1200

    Implementing Expression Stuff

--- src/expression/ExpressionHandler.java
+++ src/expression/ExpressionHandler.java
@@ -1,7 +1,9 @@
 package expression;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import tptb.Game;
 import tptb.VarBlock;
@@ -9,16 +11,45 @@ import tptb.VarBlock;
 public class ExpressionHandler {
 
 	private Game game;
+	String[] expr;
+	String[] prefix;
 	
-	public ExpressionHandler(Game game, String expression){
+	public ExpressionHandler(Game game){
+		
 		this.game = game;
+		
+		expr = game.getExpressions();
+		prefix = new String[expr.length + 1];
+		
+		for(int i = 0; i <= expr.length; i++){
+			prefix[i] = Prefixer.infixToPrefixConvert(expr[i], false);
+		}		
 	}
 	
 	public boolean evaluateExpression(){
 		Map<VarBlock, Integer> map = getValues();
-		//TODO
-		return false;
-	}
+		
+		String[] subExpr = new String[prefix.length+1];
+		
+		System.out.println(prefix);
+		
+		for(int i = 0; i < prefix.length+1; i++){
+			for(Map.Entry<VarBlock, Integer> m : map.entrySet()){
+				//NOTE: If the value is >9 it will be a value of its first digit.
+				subExpr[i] = prefix[i].replace(m.getKey().getName(), 
+						m.getValue().toString().toCharArray()[0]);
+			}
+		}
+		System.out.println(subExpr);
+		
+		Double[] vals = new Double[subExpr.length+1];
+		
+		for(int i = 0; i <= subExpr.length; i++){
+			vals[i] = PrefixEvaluator.evaluate(new Scanner(subExpr[i]));
+		}
+		
+		return vals[0] == vals[1];
+}
 	
 	private Map<VarBlock, Integer> getValues(){
 		
---
+++ src/expression/PrefixEvaluator.java
@@ -0,0 +1,41 @@
+package expression;
+
+import java.util.*;
+
+//Credit: http://www.buildingjavaprograms.com/code-files/4ed/ch12/PrefixEvaluator.java
+
+public class PrefixEvaluator {
+
+	// pre : input contains a legal prefix expression
+    // post: expression is consumed and the result is returned
+    public static double evaluate(Scanner input) {
+        if (input.hasNextDouble()) {
+            return input.nextDouble();
+        } else {
+            String operator = input.next();
+            double operand1 = evaluate(input);
+            double operand2 = evaluate(input);
+            return evaluate(operator, operand1, operand2);
+        }
+    }
+
+    // pre : operator is one of +, -, *, / or %
+    // post: returns the result of applying the given operator to
+    //       the given operands
+    public static double evaluate(String operator, double operand1,
+                                  double operand2) {
+        if (operator.equals("+")) {
+            return operand1 + operand2;
+        } else if (operator.equals("-")) {
+            return operand1 - operand2;
+        } else if (operator.equals("*")) {
+            return operand1 * operand2;
+        } else if (operator.equals("/")) {
+            return operand1 / operand2;
+        } else if (operator.equals("%")) {
+            return operand1 % operand2;
+        } else {
+            throw new RuntimeException("illegal operator " + operator);
+        }
+    }
+}
