2f81252b32f051f87671ebebf1ecc405e6384abf
Elliot Wilde <elliotw@hotmail.com>
2016-04-29 23:10:36 +1200

    Added expression getter stub for Aran

--- src/tptb/Game.java
+++ src/tptb/Game.java
@@ -32,8 +32,24 @@ public class Game implements UIKeyListener{
 		new Game();
 	}
 	
-	public boolean moveable(Player pl, Direction dir){
-		return false;
+	public Entity moveable(Entity en, Direction dir){
+		Loc enLoc = en.getLocation();
+		int enx = enLoc.getX(), eny = enLoc.getY();
+		int targetx = enx, targety = eny;
+		switch (dir){
+			case Down: targety++; break;
+			case Up: targety--; break;
+			case Left: targetx++; break;
+			case Right: targety--; break;
+		}
+		Loc targetLoc = new Loc(targetx, targety);
+		VarBlock target = null;
+		for (VarBlock v: onBoard){
+			if (false);
+		}
+		
+		
+		return null;
 	}
 	
 	//----------------------------------------------
@@ -52,6 +68,10 @@ public class Game implements UIKeyListener{
 		return board[loc.getX()][loc.getY()];
 	}
 	
+	public String[] getExpressions(){ // TODO: this
+		return null;
+	}
+	
 	//----------------------------------------------
 	//Input Handler
 	//----------------------------------------------
@@ -63,17 +83,27 @@ public class Game implements UIKeyListener{
 		if(players.length != 2)
 			System.out.println("There is not two players");
 		
+		Direction d = null;
+		Player p = null;
+		
 		switch(key){
-			case "w": 		players[0].move(Direction.Up); break;
-			case "s": 		players[0].move(Direction.Down); break;
-			case "a": 		players[0].move(Direction.Left); break;
-			case "d": 		players[0].move(Direction.Right); break;
+			case "w": 		d = Direction.Up; p = players[0]; break;
+			case "s": 		d = Direction.Down; p = players[0]; break;
+			case "a": 		d = Direction.Left; p = players[0]; break;
+			case "d": 		d = Direction.Right; p = players[0]; break;
 			
-			case "Up": 		players[1].move(Direction.Up); break;
-			case "Down": 	players[1].move(Direction.Down); break;
-			case "Left": 	players[1].move(Direction.Left); break;
-			case "Right": 	players[1].move(Direction.Right); break;
+			case "Up": 		d = Direction.Up; p = players[1]; break;
+			case "Down": 	d = Direction.Down; p = players[1]; break;
+			case "Left": 	d = Direction.Left; p = players[1]; break;
+			case "Right": 	d = Direction.Right; p = players[1]; break;
 			default:
+		}
+		Entity e = moveable(p, d);
+		if ((e = moveable(p, d))!=null){
+			if (e instanceof VarBlock){
+				e.move(d);
+			}
+			p.move(d);
 		}
 		
 		
