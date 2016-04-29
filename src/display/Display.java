c0c6004ea356348d54e60dc3be7efe91b2262bf3
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2016-04-29 19:36:22 +1200

    Inital Update display to work with input data

--- src/display/Display.java
+++ src/display/Display.java
@@ -1,38 +1,47 @@
 package display;
 
+import java.util.ArrayList;
+
 import ecs100.*;
+import tptb.*;
 
 public class Display {
-	
-	private static int GRID_WIDTH = 11;
-	private static int GRID_HEIGHT = 10;
-	
+			
 	private static int GAME_SPACE_WIDTH = UI.getCanvasWidth();
 	private static int GAME_SPACE_HEIGHT = UI.getCanvasHeight();
 	
-	public Display(){
-		drawMap();
+	
+	
+	public Display(Tile[][] map, ArrayList<Entity> ent){
+		updateDisplay(map, ent);
 	}
 	
-	private void drawMap(){		
-		
-		for(int x = 0; x < GRID_WIDTH; x++){
-			for(int y = 0; y < GRID_HEIGHT; y++){
+	private void updateDisplay(Tile[][] map, ArrayList<Entity> ent){
+		for(int x = 0; x < map.length; x++){
+			for(int y = 0; y < map[x].length; y++){
 				
-				double tileWidth = GAME_SPACE_WIDTH/GRID_WIDTH;
-				double tileHeight = GAME_SPACE_HEIGHT/GRID_HEIGHT;
 				
-				if(x != GRID_WIDTH/2){
-					UI.drawRect(x*tileWidth, y*tileHeight, tileWidth, tileHeight);
-				}
-				else{
-					UI.fillRect(x*tileWidth, y*tileHeight, tileWidth, tileHeight);
-				}
-					
-					
 				
+				drawTile(map, x, y);
+				
+				drawEntity(ent, x, y);
+			}
+		}
+	}
+	
+	private void drawEntity(ArrayList<Entity> ent, int x, int y){		
+		for(Entity e : ent){
+			if(e.getLocation().equals(new Loc(x, y))){
 				
 			}
 		}
+		
+	}
+	
+	private void drawTile(Tile[][] map, int x, int y){
+		double tileWidth = GAME_SPACE_WIDTH/map.length;
+		double tileHeight = GAME_SPACE_HEIGHT/map[x].length;
+		
+		UI.drawRect(x*tileWidth, y*tileHeight, tileWidth, tileHeight);
 	}
 }
