758486a3ee994db91b40ab5ee10f6ed635ddca8b
Aran Mason <ang3l.0f.m3rcy@gmail.com>
2016-04-30 02:23:37 +1200

    fixing things

--- src/expression/ExpressionHandler.java
+++ src/expression/ExpressionHandler.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import ecs100.UI;
 import tptb.Game;
 import tptb.VarBlock;
 
@@ -19,7 +20,7 @@ public class ExpressionHandler {
 		this.game = game;
 		
 		expr = game.getExpressions();
-		prefix = new String[expr.length + 1];
+		prefix = new String[expr.length];
 		System.out.println(expr.length);
 		for(int i = 0; i < expr.length; i++){
 			prefix[i] = Prefixer.infixToPrefixConvert(expr[i], false);
@@ -29,11 +30,11 @@ public class ExpressionHandler {
 	public boolean evaluateExpression(){
 		Map<VarBlock, Integer> map = getValues();
 		
-		String[] subExpr = new String[prefix.length+1];
+		String[] subExpr = new String[prefix.length];
 		
 		System.out.println(prefix);
 		
-		for(int i = 0; i < prefix.length+1; i++){
+		for(int i = 0; i < prefix.length; i++){
 			subExpr[i] = removeBrackets(subValues(prefix[i], map));
 		}
 		System.out.println(subExpr);
@@ -41,13 +42,20 @@ public class ExpressionHandler {
 		Double[] vals = new Double[subExpr.length+1];
 		
 		for(int i = 0; i <= subExpr.length; i++){
-			vals[i] = PrefixEvaluator.evaluate(new Scanner(subExpr[i]));
+			try{
+				vals[i] = PrefixEvaluator.evaluate(new Scanner(subExpr[i]));
+			} catch(Exception e){
+				//UI.println("Need more variables");
+			}
 		}
-		
-		return vals[0] == vals[1];
+		System.out.println(vals[0] + ", " + vals[1]);
+		return vals[0] != null && vals[1] != null && vals[0] == vals[1];
 	}
 	
 	public static String subValues(String prefix, Map<VarBlock, Integer> map ){
+		
+		System.out.println("test" + prefix);
+		
 		for(Map.Entry<VarBlock, Integer> m : map.entrySet()){
 			//NOTE: If the value is >9 it will be a value of its first digit.
 			prefix = prefix.replace(m.getKey().getName(), 
@@ -57,6 +65,8 @@ public class ExpressionHandler {
 	}
 	
 	public static String removeBrackets(String s){
+		if(s == null)
+			return "";
 		return s.replaceAll("[(|)]", "");
 	}
 	
