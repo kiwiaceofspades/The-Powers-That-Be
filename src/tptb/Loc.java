59f109f6c494bd4bb183ac4be81d3fb76a642993
Elliot Wilde <elliotw@hotmail.com>
2016-04-29 18:54:44 +1200

    Added Location Class

---
+++ src/tptb/Loc.java
@@ -0,0 +1,65 @@
+package tptb;
+
+public class Loc {
+	private int x, y;
+	
+	public Loc(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+
+	public void left(){
+		x--;
+	}
+	public void right(){
+		x++;
+	}
+	public void up(){
+		y--;
+	}
+	public void down(){
+		y++;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Loc)) {
+			return false;
+		}
+		Loc other = (Loc) obj;
+		if (x != other.x) {
+			return false;
+		}
+		if (y != other.y) {
+			return false;
+		}
+		return true;
+	}	
+}
