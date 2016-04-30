64fdf2c785d552d903dd6e551f6baea41058cd78
kiwiaceofspades <alistair.eichler.ask@gmail.com>
2016-05-01 11:52:40 +1200

    Implemented mimics

--- src/tptb/Player.java
+++ src/tptb/Player.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 public class Player extends Entity{
 	
 	private String name;
+	private boolean alive;
 	
 	public Player(Loc loc, String name){
 		super(loc);
@@ -13,5 +14,11 @@ public class Player extends Entity{
 	
 	
 	public String getName(){return name;}
+
+	public boolean isAlive(){return alive;}
+	public void die() {
+		alive = false;
+		
+	}
 	
 }
